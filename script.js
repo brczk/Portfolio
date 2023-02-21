804ed30ada5faac5c1e11b923d051ac9193260d4
Skedaaa <42313348+Skedaaa@users.noreply.github.com>
2023-02-21 13:39:52 +0100

    media queries and portfolio fixes

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 let i = 0;
 const menuButton = document.querySelector('body #nav-button');
-const colors = ['#ffc20e', '#ff4a3d', '#6f3198'];
 
 const animateHome = () => {
     const nouns = ['learning', 'contribution', 'creativity', 'progression', 'success'];
-    
+    const colors = ['#ffc20e', '#ff4a3d', '#6f3198'];
     const introduction = document.querySelector('#content #home h1');
     introduction.querySelector('#nouns').textContent = `${nouns[i++ % nouns.length]}.`;
     introduction.querySelector('#iminto').style.color = `${colors[i++ % colors.length]}`;
@@ -25,23 +24,26 @@ const loadPage = (event) => {
     }
 }
 
-const toggleMenu = (e) => {
+const toggleMenu = () => {
     const navbar = document.querySelector('body #navbar');
-    if(navbar.style.display == '')
+    const content = document.querySelector('body #content');
+    if(window.getComputedStyle(navbar).display == 'none')
     {
         navbar.style.display = 'block';
+        content.style.display = 'none';
     }
     else
     {
-        navbar.style.display = '';
+        navbar.style.display = 'none';
+        content.style.display = 'block';
     }
 }
 
 const fetchPortfolio = () => {
     fetch('portfolio.txt')
     .then(response => response.text())
-    .then(content => {
-        const result = content.replace(/\n/g, '<br>');
+    .then(text => {
+        const result = text.replace(/\n/g, '<br>');
         document.querySelector('#content #about #portfolio').innerHTML = result;
     });
 }
